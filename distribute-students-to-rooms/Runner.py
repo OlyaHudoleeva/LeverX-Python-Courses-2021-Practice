93c4ecc0cf25818b7dffa723a134a9ad15bc6eb7
Volha Khudaleyeva <olga.hudoleeva98@gmail.com>
2021-05-11 12:22:49 +0300

    [refactor] remove StudentsToRoomsDistributor class

--- distribute-students-to-rooms/Runner.py
+++ distribute-students-to-rooms/Runner.py
@@ -4,26 +4,30 @@ from src.models.student import student_repository
 from src.parser import parser
 from src.util import util
 
-parser = parser.createParser()
-namespace = parser.parse_args()
 
-rooms_dir, rooms_filename = util.split_path(namespace.rooms)
-students_dir, students_filename = util.split_path(namespace.students)
+def main():
+    arg_parser = parser.createParser()
+    namespace = arg_parser.parse_args()
 
-room_reader: room_repository.RoomsRepositoryInterface = room_repository.RoomsJsonFileRepository(
-    rooms_dir, rooms_filename)
-rooms = room_reader.listRooms()
+    rooms_dir, rooms_filename = util.split_path(namespace.rooms)
+    students_dir, students_filename = util.split_path(namespace.students)
 
-student_reader: student_repository.StudentsRepositoryInterface = student_repository.StudentsJsonFileRepository(
-    students_dir, students_filename)
-students = student_reader.listStudents()
+    room_reader: room_repository.RoomsRepositoryInterface = room_repository.RoomsJsonFileRepository(
+        rooms_dir, rooms_filename)
+    rooms = room_reader.listRooms()
 
-distributor = students_to_rooms.StudentsToRoomsDistributor(rooms, students)
+    student_reader: student_repository.StudentsRepositoryInterface = student_repository.StudentsJsonFileRepository(
+        students_dir, students_filename)
+    students = student_reader.listStudents()
 
-rooms_with_students = distributor.destribute()
+    rooms_with_students = students_to_rooms.destribute(rooms, students)
 
-if namespace.format == "json":
-    room_reader.saveRooms("rooms_with_students.json", rooms_with_students)
-elif namespace.format == "xml":
-    xml_room_reader = room_repository.RoomsXmlFileRepository("resources", "rooms.xml")
-    xml_room_reader.saveRooms("rooms_with_students.xml", rooms_with_students)
+    if namespace.format == "json":
+        room_reader.saveRooms("rooms_with_students.json", rooms_with_students)
+    elif namespace.format == "xml":
+        xml_room_reader = room_repository.RoomsXmlFileRepository("resources", "rooms.xml")
+        xml_room_reader.saveRooms("rooms_with_students.xml", rooms_with_students)
+
+
+if __name__ == "__main__":
+    main()
--- distribute-students-to-rooms/src/students_to_rooms.py
+++ distribute-students-to-rooms/src/students_to_rooms.py
@@ -1,24 +1,15 @@
-class StudentsToRoomsDistributor:
+def __add_student_to_room(room, student):
+    student_without_room = {"id": student['id'], "name": student['name']}
+    if "students" not in room:
+        room["students"] = [student_without_room]
+    else:
+        room["students"].append(student_without_room)
 
-    def __init__(self, rooms, students):
-        self.rooms = rooms
-        self.students = students
 
-    def __add_student_to_room(self, room, student):
-        student_without_room = {"id": student['id'], "name": student['name']}
-        if "students" not in room:
-            room["students"] = [student_without_room]
-        else:
-            room["students"].append(student_without_room)
-
-    def destribute(self):
-        for student in self.students:
-            for room in self.rooms:
-                if student["room"] == room["id"]:
-                    self.__add_student_to_room(room, student)
-                    break
-
-        # for room in self.rooms:
-        #     room["students"] = list(filter(lambda student: student["room"] == room["id"], self.students))
-
-        return self.rooms
+def destribute(rooms, students):
+    for student in students:
+        for room in rooms:
+            if student["room"] == room["id"]:
+                __add_student_to_room(room, student)
+                break
+    return rooms
