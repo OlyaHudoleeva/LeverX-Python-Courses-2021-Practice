75c8622e60457dcd335259a9b5872970bc4bd829
Volha Khudaleyeva <olga.hudoleeva98@gmail.com>
2021-05-17 13:01:21 +0300

    [fix] issues

--- distribute-students-to-rooms/Runner.py
+++ distribute-students-to-rooms/Runner.py
@@ -1,32 +1,32 @@
 from src import students_to_rooms
-from src.models.room import room_repository
-from src.models.student import student_repository
+from src.models.room.room_repository import RoomsRepositoryInterface, RoomsJsonFileRepository, RoomsXmlFileRepository
+from src.models.student.student_repository import StudentsRepositoryInterface, StudentsJsonFileRepository
 from src.parser import parser
 from src.util import util
 
 
 def main():
-    arg_parser = parser.createParser()
+    arg_parser = parser.create_parser()
     namespace = arg_parser.parse_args()
 
     rooms_dir, rooms_filename = util.split_path(namespace.rooms)
     students_dir, students_filename = util.split_path(namespace.students)
 
-    room_reader: room_repository.RoomsRepositoryInterface = room_repository.RoomsJsonFileRepository(
+    room_reader: RoomsRepositoryInterface = RoomsJsonFileRepository(
         rooms_dir, rooms_filename)
-    rooms = room_reader.listRooms()
+    rooms = room_reader.list_rooms()
 
-    student_reader: student_repository.StudentsRepositoryInterface = student_repository.StudentsJsonFileRepository(
+    student_reader: StudentsRepositoryInterface = StudentsJsonFileRepository(
         students_dir, students_filename)
-    students = student_reader.listStudents()
+    students = student_reader.list_students()
 
     rooms_with_students = students_to_rooms.destribute(rooms, students)
 
     if namespace.format == "json":
-        room_reader.saveRooms("rooms_with_students.json", rooms_with_students)
+        room_reader.save_rooms("rooms_with_students.json", rooms_with_students)
     elif namespace.format == "xml":
-        xml_room_reader = room_repository.RoomsXmlFileRepository("resources", "rooms.xml")
-        xml_room_reader.saveRooms("rooms_with_students.xml", rooms_with_students)
+        xml_room_reader = RoomsXmlFileRepository("resources", "rooms.xml")
+        xml_room_reader.save_rooms("rooms_with_students.xml", rooms_with_students)
 
 
 if __name__ == "__main__":
--- distribute-students-to-rooms/src/models/room/room_repository.py
+++ distribute-students-to-rooms/src/models/room/room_repository.py
@@ -1,15 +1,18 @@
 import json
+from abc import ABC, abstractmethod
 
 from dicttoxml import dicttoxml
 
 
-class RoomsRepositoryInterface:
+class RoomsRepositoryInterface(ABC):
 
-    def listRooms(self):
-        print('RoomsRepositoryInterface.listRooms is called')
+    @abstractmethod
+    def list_rooms(self):
+        print('RoomsRepositoryInterface.list_rooms is called')
 
-    def saveRooms(self, new_filename, rooms):
-        pass
+    @abstractmethod
+    def save_rooms(self, new_filename, rooms):
+        print('RoomsRepositoryInterface.save_rooms is called')
 
 
 class RoomsFileRepositoryInterface(RoomsRepositoryInterface):
@@ -18,34 +21,34 @@ class RoomsFileRepositoryInterface(RoomsRepositoryInterface):
         self.dir = dir
         self.filename = filename
 
-    def listRooms(self):
-        print('RoomsFileRepositoryInterface.listRooms is called')
+    def list_rooms(self):
+        print('RoomsFileRepositoryInterface.list_rooms is called')
 
-    def saveRooms(self, new_filename, rooms):
-        pass
+    def save_rooms(self, new_filename, rooms):
+        print('RoomsFileRepositoryInterface.save_rooms is called')
 
 
 class RoomsJsonFileRepository(RoomsFileRepositoryInterface):
 
-    def listRooms(self):
-        print('RoomsJsonFileRepository.listRooms is called')
+    def list_rooms(self):
+        print('RoomsJsonFileRepository.list_rooms is called')
 
         with open(self.dir + '/' + self.filename, mode='r') as file:
             file_data = json.load(file)
             rooms = {room['id']: room for room in file_data}
             return rooms
 
-    def saveRooms(self, new_filename, rooms):
+    def save_rooms(self, new_filename, rooms):
         with open(self.dir + '/' + new_filename, 'w') as new_json_file:
             json.dump(list(rooms.values()), new_json_file)
 
 
 class RoomsXmlFileRepository(RoomsFileRepositoryInterface):
 
-    def listRooms(self):
-        print('RoomsXmlFileRepository.listRooms is called')
+    def list_rooms(self):
+        print('RoomsXmlFileRepository.list_rooms is called')
 
-    def saveRooms(self, new_filename, rooms):
+    def save_rooms(self, new_filename, rooms):
         with open(self.dir + '/' + new_filename, "w") as new_xml_file:
             xml = dicttoxml(list(rooms.values()), item_func=lambda item: 'student').decode()
             new_xml_file.write(xml)
